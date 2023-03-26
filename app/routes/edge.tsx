f0d7c70dc8f1e24b8cee0d9ef18775258ae6cf61
Leonid Pyrlia <Leo4815162342@users.noreply.github.com>
2023-03-26 22:48:06 +0200

    Update edge.tsx

--- app/routes/edge.tsx
+++ app/routes/edge.tsx
@@ -1,5 +1,17 @@
 import { json } from "@vercel/remix";
 
+function generateData() {
+  const data = [];
+  for (let i = 0; i < 10000; i++) {
+    data.push({
+      id: i,
+      name: `name ${i}`,
+      email: `email ${i}`,
+    });
+  }
+  return data;
+}
+
 export const config = { runtime: "edge" };
 
 // This `loader` now runs as an Edge Function
@@ -9,5 +21,5 @@ export async function loader({ request }) {
   // including powerful geolocation headers added by Vercel
   const city = request.headers.get("x-vercel-ip-city");
 
-  return json({ city });
+  return json({ city, data: generateData() });
 }
